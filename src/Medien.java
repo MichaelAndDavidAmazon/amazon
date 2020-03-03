d2a91e5ec98aed13beaaa8dee717c4f67faef72b
Kobishu <61743741+Kobishu@users.noreply.github.com>
2020-03-03 18:45:32 +0100

    Update Medien.java

--- src/Medien.java
+++ src/Medien.java
@@ -12,4 +12,34 @@ public class Medien {
         this.urheber = urheber;
         this.genre = genre;
     }
+     public String getTitel() {
+        return titel;
+    }
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+    public String getKommentar() {
+        return kommentar;
+    }
+    public void setKommentar(String kommentar) {
+        this.kommentar = kommentar;
+    }
+    public String getErscheinungsdatum() {
+        return erscheinungsdatum;
+    }
+    public void setErscheinungsdatum(String erscheinungsdatum) {
+        this.erscheinungsdatum = erscheinungsdatum;
+    }
+    public Urheber getUrheber() {
+        return urheber;
+    }
+    public void setUrheber(Urheber urheber) {
+        this.urheber = urheber;
+    }
+    public Genre getGenre() {
+        return genre;
+    }
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
 }
